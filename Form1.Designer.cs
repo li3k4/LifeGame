44cb4f47cf3977659cb95b6f0084d4b1e211467b
li3k4 <smalliz.ua@gmail.com>
2021-05-18 18:42:15 +0300

    Initialization

---
+++ Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿
+using System;
+
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.bStop = new System.Windows.Forms.Button();
+            this.bStart = new System.Windows.Forms.Button();
+            this.nudDens = new System.Windows.Forms.NumericUpDown();
+            this.lDen = new System.Windows.Forms.Label();
+            this.nudRes = new System.Windows.Forms.NumericUpDown();
+            this.lRes = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudDens)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudRes)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.bStop);
+            this.splitContainer1.Panel1.Controls.Add(this.bStart);
+            this.splitContainer1.Panel1.Controls.Add(this.nudDens);
+            this.splitContainer1.Panel1.Controls.Add(this.lDen);
+            this.splitContainer1.Panel1.Controls.Add(this.nudRes);
+            this.splitContainer1.Panel1.Controls.Add(this.lRes);
+            this.splitContainer1.Panel1.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.pictureBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(1391, 778);
+            this.splitContainer1.SplitterDistance = 192;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // bStop
+            // 
+            this.bStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.bStop.Location = new System.Drawing.Point(27, 233);
+            this.bStop.Name = "bStop";
+            this.bStop.Size = new System.Drawing.Size(120, 39);
+            this.bStop.TabIndex = 5;
+            this.bStop.Text = "Stop";
+            this.bStop.UseVisualStyleBackColor = true;
+            this.bStop.Click += new System.EventHandler(this.bStop_Click);
+            // 
+            // bStart
+            // 
+            this.bStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.bStart.Location = new System.Drawing.Point(27, 188);
+            this.bStart.Name = "bStart";
+            this.bStart.Size = new System.Drawing.Size(120, 39);
+            this.bStart.TabIndex = 4;
+            this.bStart.Text = "Start";
+            this.bStart.UseVisualStyleBackColor = true;
+            this.bStart.Click += new System.EventHandler(this.bStart_Click);
+            // 
+            // nudDens
+            // 
+            this.nudDens.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.nudDens.Location = new System.Drawing.Point(26, 133);
+            this.nudDens.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudDens.Name = "nudDens";
+            this.nudDens.Size = new System.Drawing.Size(120, 30);
+            this.nudDens.TabIndex = 3;
+            this.nudDens.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.nudDens.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // lDen
+            // 
+            this.lDen.AutoSize = true;
+            this.lDen.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lDen.Location = new System.Drawing.Point(23, 104);
+            this.lDen.Name = "lDen";
+            this.lDen.Size = new System.Drawing.Size(77, 25);
+            this.lDen.TabIndex = 2;
+            this.lDen.Text = "Density";
+            // 
+            // nudRes
+            // 
+            this.nudRes.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.nudRes.Location = new System.Drawing.Point(26, 57);
+            this.nudRes.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.nudRes.Name = "nudRes";
+            this.nudRes.Size = new System.Drawing.Size(120, 30);
+            this.nudRes.TabIndex = 1;
+            this.nudRes.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.nudRes.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            // 
+            // lRes
+            // 
+            this.lRes.AutoSize = true;
+            this.lRes.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lRes.Location = new System.Drawing.Point(23, 28);
+            this.lRes.Name = "lRes";
+            this.lRes.Size = new System.Drawing.Size(103, 25);
+            this.lRes.TabIndex = 0;
+            this.lRes.Text = "Resolution";
+            this.lRes.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1191, 774);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 40;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1391, 778);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.nudDens)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudRes)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.NumericUpDown nudRes;
+        private System.Windows.Forms.Label lRes;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.NumericUpDown nudDens;
+        private System.Windows.Forms.Label lDen;
+        private System.Windows.Forms.Button bStart;
+        private System.Windows.Forms.Button bStop;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private int currGen = 0;
+        private Graphics graphics;
+        private int resolve;
+        private bool[,] field;
+        private int rows;
+        private int columns;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void StartGame()// заполение первичных клеток
+        {
+
+            if (timer1.Enabled)
+                return;
+            currGen = 0;
+            Text = $"Generation{currGen}";
+
+            nudRes.Enabled = false;
+            nudDens.Enabled = false;
+            resolve = (int)nudRes.Value;
+            rows = pictureBox1.Height/resolve;
+            columns = pictureBox1.Width/resolve;
+            field = new bool[columns, rows];
+
+            Random rand = new Random();
+
+            for(int x =0; x<columns; x++) 
+            {
+                for(int y = 0; y < rows; y++)
+                {
+                    field[x, y] = rand.Next((int)nudDens.Value) == 0;
+                }
+            }
+
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            graphics = Graphics.FromImage(pictureBox1.Image);
+            timer1.Start();
+        }
+
+        private void NextGeneraton()
+        {
+            graphics.Clear(Color.Black);
+            var newField = new bool[columns, rows];
+            for (int x = 0; x < columns; x++)
+            {
+                for (int y = 0; y < rows; y++)
+                {
+                    var neighCount = CountNeighbours(x, y);
+                    var hasLife = field[x, y];
+
+                    if (!hasLife && neighCount==3)
+                        newField[x, y] = true;
+                     else if(hasLife &&(neighCount<2 || neighCount> 3))
+                        newField[x, y] = false;
+                    else
+                        newField[x, y] = field[x,y];
+
+                    if (hasLife)
+                        graphics.FillRectangle(Brushes.Blue, x * resolve, y * resolve, resolve-1, resolve-1);
+                }
+            }
+            field = newField; 
+            pictureBox1.Refresh();
+            Text = $"Generation{++currGen}";
+        }
+
+        private int CountNeighbours(int x, int y)
+        {
+            int count = 0;
+
+            for(int i =-1; i<2; i++)
+            {
+                for (int j = -1; j < 2; j++)
+                {
+                    var col = (x + i + columns) % columns ;
+                    var row = (y + j + rows) % rows;
+
+                    var isSelfChecking = col == x && row == y;
+                    var hasLife = field[col, row];
+
+                    if (hasLife && !isSelfChecking)
+                        count++;
+
+                }
+            }
+            return count;
+        }
+
+        private void StopGame()
+        {
+            if (!timer1.Enabled)
+                return;
+            timer1.Stop();
+            nudRes.Enabled = true;
+            nudDens.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            NextGeneraton();
+        }
+       
+
+        private void bStart_Click(object sender, EventArgs e)
+        {
+            StartGame();
+        }
+
+        private void bStop_Click(object sender, EventArgs e)
+        {
+            StopGame();
+        }
+
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!timer1.Enabled)
+                return;
+
+            if(e.Button == MouseButtons.Left)
+            {
+                var x = e.Location.X / resolve;
+                var y = e.Location.Y / resolve;
+                var valPass = ValidateMousPos(x,y);
+                if(valPass)
+                field[x, y] = true; 
+            }
+
+            if (e.Button == MouseButtons.Right)
+            {
+                var x = e.Location.X / resolve;
+                var y = e.Location.Y / resolve;
+                 var valPass = ValidateMousPos(x,y);
+                if(valPass)
+                field[x, y] = true; 
+            }
+
+        }
+        private bool ValidateMousPos(int x,int y)
+        {
+            return x >=0 && y >=0 && x<columns && y< rows;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Text = $"Generation{currGen}";
+        }
+    }
+}
